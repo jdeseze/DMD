eab8b1c160d1a7ef3d00d96b7d8db53a9310af1c
jdeseze <68115566+jdeseze@users.noreply.github.com>
2021-12-15 16:21:27 +0100

    Add files via upload

---
+++ Lightcrafter.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 15 14:53:22 2021
+
+@author: Jean
+
+PREVIOUS AUTHOR (matlab): Jan Winter, TU Berlin, FG Lichttechnik j.winter@tu-berlin.de
+LICENSE: free to use at your own risk. Kudos appreciated.
+"""
+import socket
+import numpy as np
+
+def convert_bits(a):
+    bnr = bin(a).replace('0b','')
+    x = bnr[::-1] #this reverses an array
+    while len(x) < 8:
+        x += '0'
+    bnr = x[::-1]
+    return bnr    
+
+class Lightcrafter:
+        
+    def __init__(self,tcp_ip,port):
+        self.tcp_ip=tcp_ip
+        self.port=port
+        self.socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(1)
+
+    def connect(self):
+        TCP_IP = self.tcp_ip
+        TCP_PORT = self.port
+        try:
+            self.socket.connect((TCP_IP, TCP_PORT))
+        except:
+            return False
+        return True
+
+    def disconnect(self):
+        try:
+            self.socket.close()
+        except:
+            pass
+    
+    def createHeader(self):
+        return np.uint8(np.zeros(6))
+    
+    def appPayloadLengthToHeaderForPayload( self, header, payload ):
+        payloadLength = len(payload)
+        payloadLengthMSB = int(payloadLength/256)
+        payloadLengthLSB = np.mod(payloadLength,256)
+        
+        header[4] = np.uint8( payloadLengthLSB ) #payloadLength LSB
+        header[5] = np.uint8( payloadLengthMSB ) #payloadLength MSB
+        
+        return header
+    
+    def appChecksum( self,packet):
+        checksum = np.mod( np.sum( packet ), 256 )
+        return np.append(packet,checksum)
+    
+    def setdisplayModeStatic( self):
+        header = self.createHeader()
+        header[0] = np.uint8( 0x02 )	#packet type
+        header[1] = np.uint8( 0x01 ) #CMD1
+        header[2] = np.uint8( 0x01 ) #CMD2
+        header[3] = np.uint8( 0x00 ) #flags
+        header[4] = np.uint8( 0x01 ) #payloadLength LSB
+        header[5] = np.uint8( 0x00 ) #payloadLength MSB
+        payload = np.uint8( 0x00 ) #payload
+        packet = self.appChecksum(np.append(header,payload) )
+        #packet
+        self.sendData( packet)
+    
+    def setdisplayModeInternalPattern(self):
+        header = self.createHeader()
+        header[0] = np.uint8( 0x02 )	#packet type
+        header[1] = np.uint8( 0x01 ) #CMD1
+        header[2] = np.uint8( 0x01 ) #CMD2
+        header[3] = np.uint8( 0x00 ) #flags
+        header[4] = np.uint8( 0x01 ) #payloadLength LSB
+        header[5] = np.uint8( 0x00 ) #payloadLength MSB
+        payload = np.uint8( 0x01 ) #payload
+        packet = self.appChecksum( np.append(header,payload) )
+        #packet
+        self.sendData( packet)
+
+    def setdisplayModePatternSequence(self):
+        header = self.createHeader()
+        header[0] = np.uint8( 0x02 )	#packet type
+        header[1] = np.uint8( 0x01 ) #CMD1
+        header[2] = np.uint8( 0x01 ) #CMD2
+        header[3] = np.uint8( 0x00 ) #flags
+        header[4] = np.uint8( 0x01 ) #payloadLength LSB
+        header[5] = np.uint8( 0x00 ) #payloadLength MSB
+        payload = np.uint8( 0x04 ) #payload
+        packet = self.appChecksum( np.append(header,payload) )
+        #packet
+        self.sendData( packet)
+    
+    def setdisplayPatternSequenceSettings(self,bit_depth=1,nb_pattern=4):
+        header = self.createHeader()
+        header[0] = np.uint8( 0x02 )	#packet type
+        header[1] = np.uint8( 0x04 ) #CMD1
+        header[2] = np.uint8( 0x00 ) #CMD2
+        header[3] = np.uint8( 0x00 ) #flags
+        
+        exp=[0x11,0x10,0x01,0x10]
+        trig=[0x00,0x00,0x00,0x00]+[0x00,0x00,0x00,0x00]
+        payload = np.uint8( [0x01,0x04,0x01,0x00]+trig+exp+[0x02]) #payload
+        
+        self.appPayloadLengthToHeaderForPayload(header, payload)
+        packet = self.appChecksum( np.append(header,payload) )
+        #packet
+        self.sendData( packet)
+    
+    def setPattern( self, pattern):
+        #self.setdisplayModeInternalPattern()
+        
+        header = self.createHeader()
+        header[0] = np.uint8( 0x02 )	#packet type
+        header[1] = np.uint8( 0x01 ) #CMD1
+        header[2] = np.uint8( 0x03 ) #CMD2
+        header[3] = np.uint8( 0x00 ) #flags
+        header[4] = np.uint8( 0x01 ) #payloadLength LSB
+        header[5] = np.uint8( 0x00 ) #payloadLength MSB
+        payload = np.uint8( pattern ) #payload
+        packet = self.appChecksum( np.append(header,payload) )
+        #packet
+        self.sendData( packet)
+    
+    def setStaticColor( self, R, G, B):
+        self.setdisplayModeStatic()
+        
+        header = self.createHeader()
+        header[0] = np.uint8( 0x02 )	#packet type
+        header[1] = np.uint8( 0x01 ) #CMD1
+        header[2] = np.uint8( 0x06 ) #CMD2
+        header[3] = np.uint8( 0x00 ) #flags
+        header[4] = np.uint8( 0x03 ) #payloadLength LSB
+        header[5] = np.uint8( 0x00 ) #payloadLength MSB
+        
+        payload = np.uint8( np.array([R,G,B]) ) #payload
+
+        packet = self.appChecksum( np.append(header,payload) )
+        #packet
+        self.sendData( packet)
+            
+    
+    def setBMPImage( self, imageData):
+        #self.setdisplayModeStatic()
+        
+        MAX_PAYLOAD_SIZE = 65535
+        numberOfChunks = np.ceil( imageData.size / 65535 )
+        #print(numberOfChunks)
+# =============================================================================
+#         for i in range(int(numberOfChunks)):
+#             currentLength = imageData.size
+#             if( currentLength > MAX_PAYLOAD_SIZE ):
+#                 chunkArray.append(imageData[0 : MAX_PAYLOAD_SIZE])
+#                 imageData = imageData[MAX_PAYLOAD_SIZE :]
+#             else:
+#                 chunkArray.append(imageData[0:])
+#             
+#         for currentChunkIndex in range(int(numberOfChunks)):
+#             
+#             currentChunk = chunkArray[currentChunkIndex]
+# =============================================================================
+        for currentChunkIndex in range(int(numberOfChunks)):
+            
+            currentLength = imageData.size
+            
+            if( currentLength > MAX_PAYLOAD_SIZE ):
+                currentChunk=imageData[0 : MAX_PAYLOAD_SIZE]
+                imageData = imageData[MAX_PAYLOAD_SIZE :]
+            else:
+                currentChunk=imageData           
+            
+            
+            header = self.createHeader()
+            header[0] = np.uint8( 0x02 )	#packet type
+            header[1] = np.uint8( 0x01 ) #CMD1
+            header[2] = np.uint8( 0x05 ) #CMD2
+            header = self.appPayloadLengthToHeaderForPayload( header, currentChunk )
+            
+            #app flag
+            if( numberOfChunks == 1 ):
+                header[3] = np.uint8( 0x00 ) #flags
+            else:
+                if( currentChunkIndex == 0 ):
+                    #print('FIRST CHUNK')
+                    header[3] = np.uint8( 0x01 ) #flags
+                elif( currentChunkIndex == numberOfChunks-1 ):
+                    #print('LAST CHUNK')
+                    header[3] = np.uint8( 0x03 ) #flags
+                else:
+                    #print('OTHER CHUNK')
+                    header[3] = np.uint8( 0x02 ) #flags
+            
+            packet = self.appChecksum(np.append(header,currentChunk))
+            self.sendData( packet)
+
+    def sendData( self, packet):
+        #limit packet size
+        MAX_SIZE = 512
+        buffer = packet
+        #print(packet)
+        while buffer.any():
+            if( len(buffer) > MAX_SIZE ):
+                currentPacket = buffer[0 : MAX_SIZE]
+                buffer = buffer[MAX_SIZE:]        
+            else:
+                currentPacket = buffer
+                buffer = np.array([])
+
+            #print(bytes(list(currentPacket)))
+            self.socket.send(bytes(list(currentPacket))) 
+            #print('wrote some data')
---
+++ connectingDMD.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 17 11:36:19 2021
+
+@author: Jean
+"""
+import streamlit as st
+import socket
+from Lightcrafter import Lightcrafter
+from PIL import Image
+import numpy as np
+import pycromanager as pm
+#pip install pythonnet
+import clr
+import matplotlib.pyplot as plt
+from streamlit_drawable_canvas import st_canvas
+import time
+import copy
+
+def main():
+    st.set_page_config(page_title="DMD", page_icon=":microscope:",layout="wide")
+    
+    #Initialize variables
+    
+    #dmdzone is tthe mask of the zone of the dmd in the field of view of the app (defined as 512,512)
+    if 'dmd_zone' not in st.session_state:
+        st.session_state.dmd_zone=np.ones((512,512))
+    if 'cropped_index' not in st.session_state:
+            st.session_state.cropped_index=np.ix_((st.session_state.dmd_zone>0).any(1),(st.session_state.dmd_zone>0).any(0))
+        
+    if 'show_image' not in st.session_state:
+        st.session_state['show_image']=False
+    
+    #dmd is what will be sent to the dmd
+    if 'dmd' not in st.session_state:
+        st.session_state.dmd=np.zeros((int(1140/2),912))
+    
+    c1,c2,c3=st.columns([1,2,1])
+    with c1:
+        st.session_state.soft=st.radio('Software',['Metamorph','Micromanager'])
+        
+        if st.session_state.soft=='Metamorph':
+            clr.AddReference(r'D:\JEAN\DMD\Interop.MMAppLib.dll')
+            import MMAppLib
+            st.session_state.mm=MMAppLib.UserCallClass()
+            
+        if st.button('Connect DMD'):
+            TCP_IP = '192.168.7.2'
+            TCP_PORT = 0x5555
+            
+            st.session_state.L=Lightcrafter(TCP_IP,TCP_PORT)
+            st.session_state.Lstate=st.session_state.L.connect()
+            if st.session_state.Lstate:
+                st.write('DMD connected')
+                #st.session_state.L.setStaticColor(0xf,0xf,0xf)
+                st.session_state.L.setdisplayModeStatic()
+            else: 
+                st.write('Unable to connect DMD')
+        
+        if st.button('Send pattern to DMD'):
+            send_pattern()
+        
+        if st.button('Acquire'):
+            acquire()
+        
+        st.session_state.drawing_mode = st.selectbox(
+        "Drawing tool:", ("freedraw", "line", "rect", "circle", "transform")
+        )
+        st.session_state.stroke_width = st.slider("Stroke width: ", 1, 25, 3)
+        
+        if st.button('Define as DMD zone'):
+            st.session_state.dmd_zone=copy.deepcopy(st.session_state.mask_act)
+            #look at the index of the dmd_zone
+            st.session_state.cropped_index=np.ix_((st.session_state.dmd_zone>0).any(1),(st.session_state.dmd_zone>0).any(0))
+            st.session_state.mask_act=np.zeros((512,512))
+
+        if st.button('Disconnect DMD'):
+            st.session_state.L.disconnect()
+            st.session_state.Lstate=False
+        
+        
+        
+    with c2:  
+        if st.session_state.show_image:
+            make_canvas()
+            st.session_state.mask_act=np.mean(st.session_state.canvas_result.image_data,axis=2)>0
+            plt.imshow(st.session_state.mask_act,cmap='gray')
+            st.session_state.canvasfig.savefig('mask.png',bbox_inches="tight",pad_inches = 0)
+
+            cropped_image=st.session_state.mask_act[st.session_state.cropped_index].astype(np.uint8).T
+            st.session_state.dmd=np.array(Image.fromarray(cropped_image).resize((int(1140/2),912))).astype(np.uint8)
+            im=Image.fromarray(st.session_state.dmd)
+            #im.convert('L')
+            im.save('test.bmp')
+            
+    with c3:
+        if st.session_state.show_image:
+            st.image(st.session_state.dmd_zone*255,use_column_width=True,output_format='PNG')
+            st.image(st.session_state.dmd.T*255,use_column_width=True,output_format='PNG')    
+
+
+
+def acquire():
+    if st.session_state.soft=='Metamorph':
+        st.session_state.mm.RunJournal('C:/MM/app/mmproc/journals/s.JNL')
+        pixvals=np.array(Image.open(r'C:\TEMP\tmp.tif'))
+    else:
+        bridge=pm.Bridge()
+        core=bridge.get_core()
+        core.snap_image()
+        tagged_img=core.get_tagged_image()
+        pixvals=np.reshape(tagged_img.pix,newshape=[tagged_img.tags['Height'], tagged_img.tags['Width']])
+    contrasted = ((pixvals - pixvals.min()) / (pixvals.max()-pixvals.min())) 
+    st.session_state.disp_image=contrasted
+    st.session_state.img_to_save=pixvals
+    st.session_state.show_image=True  
+
+def send_pattern():
+    if not st.session_state.Lstate:
+        st.write('DMD not connected')
+    else:
+        with open('test9.bmp','rb') as opened:
+                tosend=np.fromfile(opened,np.uint8).flatten()
+        st.session_state.L.connect()
+        time.sleep(1)
+        st.session_state.L.setdisplayModeStatic()
+        time.sleep(1)
+        st.session_state.L.setBMPImage(tosend)
+        time.sleep(2)
+        st.session_state.L.setdisplayModePatternSequence()
+        time.sleep(2)
+        st.session_state.L.setdisplayPatternSequenceSettings()
+    
+def make_canvas():
+    img=np.array(st.session_state.disp_image)
+
+    size_fig=(512,512)
+    st.session_state.canvasfig = plt.figure(figsize=size_fig, dpi=1)
+    b=plt.imshow(img,cmap='gray')
+    b.axes.axis('off')
+    plt.tight_layout()
+    plt.gca().set_axis_off()
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, 
+            hspace = 0, wspace = 0)
+    plt.margins(0,0)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    st.session_state.canvasfig.savefig('canvas.png',bbox_inches="tight",pad_inches = 0)
+    bg_image = Image.open('canvas.png')
+    size_img=size_fig
+    st.session_state.canvas_result = st_canvas(
+        fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+        stroke_width=st.session_state.stroke_width,
+        stroke_color="#000000",
+        background_color="",
+        background_image=bg_image,
+        update_streamlit=True,
+        height=size_img[1],
+        width=size_img[0],
+        drawing_mode=st.session_state.drawing_mode,
+        key="canvas",
+    )
+    
+if __name__ == "__main__":
+    main()
